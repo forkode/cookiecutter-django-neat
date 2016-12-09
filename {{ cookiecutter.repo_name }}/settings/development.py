fb741f723d9f6482cf492ed8ce7f2ebd5914a526
Ruslan Zhenetl <zhenetl.r@rficb.ru>
2016-12-09 18:07:52 +0300

    Small fixes

--- {{ cookiecutter.repo_name }}/settings/development.py
+++ {{ cookiecutter.repo_name }}/settings/development.py
@@ -8,6 +8,7 @@ INSTALLED_APPS += (
     {% if cookiecutter.use_rest_framework == 'y' -%}
       'django_rest_framework_generator',
     {%- endif %}
+    '{{ cookiecutter.repo_name }}',
 )
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
