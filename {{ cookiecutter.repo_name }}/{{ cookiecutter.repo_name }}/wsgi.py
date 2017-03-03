c9255214808e369c4998ca224055cf84507d76b5
c6401 <nc6401+git@gmail.com>
2017-03-03 20:26:53 +0300

    Cleanup settings, add login template

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/wsgi.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/wsgi.py
@@ -14,7 +14,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
-    "settings.production"
+    "settings"
 )
 
 application = get_wsgi_application()
